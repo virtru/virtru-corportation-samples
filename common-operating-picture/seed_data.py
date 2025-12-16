3030e60bd5b549c60f92159a162dc7c3f5c99d21
vboxuser <patrick.hakun@missionedgetechnologies.com>
2025-12-16 16:43:57 +0000

    Updated seed with more objects

--- common-operating-picture/seed_data.py
+++ common-operating-picture/seed_data.py
@@ -19,8 +19,8 @@ DB_USER = "postgres"
 DB_PASSWORD = "changeme"
 DB_HOST = "localhost"
 DB_PORT = 15432
-NUM_RECORDS = 3 # Number of records to insert
-BATCH_SIZE = 1 # Number of records per batch
+NUM_RECORDS = 50 # Number of records to insert
+BATCH_SIZE = 10 # Number of records per batch
 
 # --- Fixed Data for TdfObjects ---
 FIXED_SRC_TYPE = 'vehicles'
